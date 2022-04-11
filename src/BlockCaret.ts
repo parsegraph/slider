1db06c4c2fd7a5f689c9b9ec190cfcc8620ec6ed
Aaron Faanes <dafrito@gmail.com>
2022-04-10 21:16:04 -0500

    Fix BlockCaret

--- src/BlockCaret.ts
+++ src/BlockCaret.ts
@@ -1,8 +1,14 @@
 import { PaintedCaret } from "parsegraph-artist";
 import Block from "./Block";
 import Font from "parsegraph-font";
+import DefaultBlockPalette from "./DefaultBlockPalette";
+import BlockPalette from "./BlockPalette";
 
 export default class BlockCaret extends PaintedCaret<Block> {
+  constructor(given?: any, palette: BlockPalette = new DefaultBlockPalette()) {
+    super(given, palette);
+  }
+
   label(label?: string, font?: Font) {
     if (arguments.length === 0) {
       return this.node().value().label();
